e6568453b08bd2c1e00b3227b238102320176b83
owain.llewellyn <bilbercode@gmail.com>
2019-03-05 14:26:22 +0000

    feat: AWS module and variable pass through Provides; * AWS * * Bucket * * Account * GCP * * module pass through

---
+++ aws/bucket/main.tf
@@ -0,0 +1,85 @@
+data "aws_iam_policy_document" "access" {
+  "statement" {
+    effect = "Allow"
+    sid = "ReadAccess"
+    actions = [
+      "s3:GetObject",
+
+    ]
+    resources = [
+      "arn:aws:s3:::${format("uw-%s-%s-%s",replace(var.team, "/^uw-/", ""), var.name, var.env)}/*"
+    ]
+    principals {
+      identifiers = ["${compact(concat(split(",",replace(var.write_access, " ", "")), split(",",replace(var.read_access, " ", ""))))}"]
+      type = "AWS"
+    }
+  }
+  "statement" {
+    effect = "Allow"
+    sid = "ListBucketContents"
+    actions = [
+      "s3:ListBucket",
+      "s3:ListBucketMultipartUploads",
+      "s3:GetBucketLocation",
+    ]
+    resources = [
+      "arn:aws:s3:::${format("uw-%s-%s-%s", replace(var.team, "/^uw-/", ""), var.name, var.env)}"
+    ]
+    principals {
+      identifiers = ["${compact(concat(split(",",replace(var.write_access, " ", "")), split(",",replace(var.list_access, " ", ""))))}"]
+      type = "AWS"
+    }
+  }
+  "statement" {
+    effect = "Allow"
+    sid = "WriteAccess"
+    actions = [
+      "s3:DeleteObject",
+      "s3:PutObject",
+      "s3:PutObjectAcl",
+      "s3:ListMultipartUploadParts",
+      "s3:AbortMultipartUpload",
+    ]
+    resources = [
+      "arn:aws:s3:::${format("uw-%s-%s-%s", replace(var.team, "/^uw-/", ""), var.name, var.env)}/*"
+    ]
+    principals {
+      identifiers = ["${compact(split(",",replace(var.write_access, " ", "")))}"]
+      type = "AWS"
+    }
+  }
+  "statement" {
+    effect = "Deny"
+    sid = "DenyUnEncryptedObjectUploads"
+    actions = [
+      "s3:PutObject"
+    ]
+    resources = [
+      "arn:aws:s3:::${format("uw-%s-%s-%s", replace(var.team, "/^uw-/", ""), var.name, var.env)}/*"
+    ]
+    principals {
+      identifiers = [
+        "*"
+      ]
+      type = "*"
+    }
+    condition {
+      test = "StringNotEquals"
+      values = [
+        "AES256"
+      ]
+      variable = "s3:x-amz-server-side-encryption"
+    }
+  }
+}
+
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "${format("uw-%s-%s-%s", replace(var.team, "/^uw-/", ""), var.name, var.env)}"
+  acl = "private"
+  policy = "${data.aws_iam_policy_document.access.json}"
+  tags {
+    terraform = "Managed by terraform"
+    team = "${var.team}"
+  }
+}
---
+++ aws/bucket/vars.tf
@@ -0,0 +1,26 @@
+variable "name" {
+  description = "the bucket name"
+}
+
+variable "team" {
+  description = "the team to which the bucket belongs"
+}
+
+variable "env" {
+  description = "the environment the bucket exists in"
+}
+
+variable "write_access" {
+  description = "arns with write access"
+  default = ""
+}
+
+variable "read_access" {
+  default = ""
+  description = "arns with read access"
+}
+
+variable "list_access" {
+  default = ""
+  description = "arns with list content access"
+}
