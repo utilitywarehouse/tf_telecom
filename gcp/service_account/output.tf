def2b58dcfa8bdcb335b35a0b2f9bf9d22e08e45
owain.llewellyn <bilbercode@gmail.com>
2019-03-05 14:53:46 +0000

    feat: additional outpu vars for GCP SvcAccount

--- gcp/service_account/output.tf
+++ gcp/service_account/output.tf
@@ -2,6 +2,14 @@ output "email" {
   value = "${google_service_account.account.email}"
 }
 
+output "name" {
+  value = "${google_service_account.account.name}"
+}
+
+output "id" {
+  value = "${google_service_account.account.id}"
+}
+
 output "private_key" {
   value = "${google_service_account_key.account.private_key}"
 }
