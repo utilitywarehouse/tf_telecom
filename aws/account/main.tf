2389c48f9631cbc65cb185952ac673748cad787a
owain.llewellyn <bilbercode@gmail.com>
2019-03-12 10:28:06 +0000

    fix: formatting issues

--- aws/account/main.tf
+++ aws/account/main.tf
@@ -1,10 +1,10 @@
 
 
 resource "aws_iam_user" "user" {
-  name = "${format("/%s", replace(var.team, "/^uw-/", ""))}-${var.name}"
-  permissions_boundary = "arn:aws:iam::${var.account_id}:policy/sys-${format("/%s", replace(var.team, "/^uw-/", ""))}-boundary"
+  name = "${format("%s", replace(var.team, "/^uw-/", ""))}-${var.name}"
+  permissions_boundary = "arn:aws:iam::${var.account_id}:policy/sys-${format("%s", replace(var.team, "/^uw-/", ""))}-boundary"
   tags {
-    team = "${format("/%s", replace(var.team, "/^uw-/", ""))}"
+    team = "${format("%s", replace(var.team, "/^uw-/", ""))}"
     terraform = "Managed by terraform"
   }
 }
