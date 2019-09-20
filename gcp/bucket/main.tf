6ac211f14616aea7e469050c8a39fcd25332748b
owain.llewellyn <bilbercode@gmail.com>
2019-09-20 13:33:25 +0100

    patch to fix interpolation on gcp bucket

--- gcp/bucket/main.tf
+++ gcp/bucket/main.tf
@@ -11,7 +11,7 @@ resource "google_storage_bucket" "bucket" {
 resource "google_storage_bucket_acl" "acl" {
   bucket = google_storage_bucket.bucket.name
   role_entity = [
-    flatten(compact(concat(
+    compact(flatten(concat(
       tolist([format("OWNER:project-owners-%s", var.project_number)]),
       formatlist(
         "WRITER:user-%s",
