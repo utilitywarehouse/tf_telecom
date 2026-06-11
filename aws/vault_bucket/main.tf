f0f122ef40f0ef67475bb14433b73d254410ce84
Josh Dysiewicz <joshua.dysiewicz@uw.co.uk>
2026-06-11 15:41:19 +0100

    add tags to vault-bucket

--- aws/vault_bucket/main.tf
+++ aws/vault_bucket/main.tf
@@ -1,46 +1,36 @@
+locals {
+  bucket_name = format(
+    "uw-%s-%s-%s",
+    replace(var.team, "/^uw-/", ""),
+    var.name,
+    var.env,
+  )
+}
+
 data aws_iam_policy_document "vault" {
   statement {
     actions = [
       "s3:*"
     ]
     resources = [
-      "arn:aws:s3:::${format(
-        "uw-%s-%s-%s",
-        replace(var.team, "/^uw-/", ""),
-        var.name,
-        var.env,
-      )}",
-      "arn:aws:s3:::${format(
-          "uw-%s-%s-%s",
-          replace(var.team, "/^uw-/", ""),
-          var.name,
-          var.env,
-        )}/*",
+      "arn:aws:s3:::${local.bucket_name}",
+      "arn:aws:s3:::${local.bucket_name}/*",
     ]
   }
 }
 
 resource aws_iam_role_policy "vault" {
-  name = format(
-        "uw-%s-%s-%s-vault",
-        replace(var.team, "/^uw-/", ""),
-        var.name,
-        var.env,
-      )
+  name = local.bucket_name
   role = var.vault_role_id
   policy = data.aws_iam_policy_document.vault.json
 }
 
 resource "aws_s3_bucket" "bucket" {
-  bucket = format(
-    "uw-%s-%s-%s",
-    replace(var.team, "/^uw-/", ""),
-    var.name,
-    var.env,
-  )
+  bucket = local.bucket_name
   tags = {
     terraform = "Managed by terraform"
     team      = var.team
+    Name = local.bucket_name
   }
 }
 
